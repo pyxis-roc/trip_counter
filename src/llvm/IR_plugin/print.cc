28b66137926e8f3dd8d59ff3b14bd71527419d4f
Jingyu Qiu <118010248@link.cuhk.edu.cn>
2025-04-21 16:09:59 -0400

    add IR plug in

--- src/llvm/IR_plugin/print.cc
+++ src/llvm/IR_plugin/print.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 extern "C" {
-    void print_counter(int block_ID, char* name, long N){
+    void print_counter(char* block_ID, char* name, long N){
         std::cout<<block_ID<<' '<<name<<' '<<N<<std::endl;
     }
 }
