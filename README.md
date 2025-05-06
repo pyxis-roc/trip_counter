8d84ddc91733025dbf03249055dae4f3b6defd00
Jingyu Qiu <118010248@link.cuhk.edu.cn>
2025-05-06 09:56:04 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# trip_counter
+# Trip Counter
+## Overview
+This is a tool to enable symbolic profiling for LLVM IR. It takes in a LLVM IR file and the target function name. Output is the instrumented LLVM IR. Calls to the target function will print execution count of its internal basic blocks.
+
+### Requirements
+- **CMake**: Version 3.10 or higher.
+- **Python**: Version 3.6 or higher.
+- **Clang**: Version 10.0 or higher.
+- **LLVM**: Version 10.0 or higher.
+
+## Building and Running
+Use CMake to build the target
+
+```bash
+$ cd src/llvm
+$ mkdir build && cd build
+$ cmake ..
+$ cmake --build .
+```
+
+Try this tool to generate instrumented IR
+
+```bash
+$ ./symCount example.ll main 2> example_instrumented.ll
+```
+
+Then when `example_instrumented.ll` is compiled and the `main` function is called. Execution counts of `main`'s internal basic blocks will be printed to stdout.
--- experiments/diff.py
+++ experiments/diff.py
@@ -1,13 +1,30 @@
 #! /usr/bin/env python3
 
 import argparse
+import sys
+import subprocess
 
-parser = argparse.ArgumentParser(description='Diff of basic block counts between instr and proxy')
+def parse_instr_raw_file(raw_file, ir_file):
+    # get text output from llvm profraw file
+    # llvm-profdata merge -output=instr.profdata instr.profraw
 
-parser.add_argument('instr', type=str, help='Path to instr.txt')
-parser.add_argument('proxy', type=str, help='Path to proxy.txt')
-
-args = parser.parse_args()
+    try:
+        prof_file = raw_file.replace(".profraw", ".profdata")
+        subprocess.run(
+            ["llvm-profdata", "merge", "-output=" + prof_file, raw_file],
+            check=True
+        )
+        # get text output from llvm profdata file
+        instr_file = prof_file.replace(".profdata", ".txt")
+        subprocess.run(
+            ["opt", "--passes=pgo-instr-use", "-pgo-test-profile-file=" + prof_file, "-pgo-view-raw-counts", "text", ir_file],
+            check=True,
+            stderr=open(instr_file, 'w')
+        )
+        return instr_file
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running llvm-profdata: {e}")
+        sys.exit(2)
 
 def get_instr_file_counts(file_name):
 
@@ -16,8 +33,11 @@ def get_instr_file_counts(file_name):
         texts = instr_file.read()
         lines = texts.split("\n")
         for line in lines:
-            if line == "":
+            if "BB:" not in line:
                 continue
+            if "FakeNode" in line:
+                continue
+
             block_name = line.split()[1]
             block_count_tk = line.split()[3]
             block_count = int(block_count_tk.split("=")[1]) 
@@ -38,10 +58,35 @@ def get_proxy_file_counts(file_name):
             block_counts[block_name] = block_count
     return block_counts
 
-instr_counts = get_instr_file_counts(args.instr)
-proxy_counts = get_proxy_file_counts(args.proxy)
+def compare(instr_counts, proxy_counts):
+    diff = 0
 
-for block in sorted(proxy_counts):
-    if block not in instr_counts: continue
-    if instr_counts[block] != proxy_counts[block]:
-        print(f"BB: {block} has different counts: \ninstr:{instr_counts[block]} vs proxy:{proxy_counts[block]}")
+    for block in sorted(instr_counts):
+        if block not in proxy_counts: continue
+        if instr_counts[block] != proxy_counts[block]:
+            diff += 1
+            print(f"BB: {block} has different counts: \ninstr:{instr_counts[block]} vs proxy:{proxy_counts[block]}")
+
+    print(f"Total different basic blocks: {diff}")
+    return diff
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Diff of basic block counts between instr and proxy')
+
+    parser.add_argument('proxy', type=str, help='Path to proxy.txt')
+    parser.add_argument('instrRaw', type=str, help='Path to default.profraw')
+    parser.add_argument('irFile', type=str, help='Path to the IR file')
+
+    args = parser.parse_args()
+
+    instr_file = parse_instr_raw_file(args.instrRaw, args.irFile)
+    instr_counts = get_instr_file_counts(instr_file)
+    proxy_counts = get_proxy_file_counts(args.proxy)
+
+    diff = compare(instr_counts, proxy_counts)
+    if diff == 0:
+        print("No differences in basic block counts")
+        sys.exit(0)
+    else:
+        sys.exit(1)
